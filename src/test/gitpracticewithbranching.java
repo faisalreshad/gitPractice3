c12377a1c22881f8bfb0af2ab248e8a83adc1769
FaisalReshad <faisal.reshad@tekschool.us>
2022-11-09 20:07:05 -0500

    @nd commit

--- src/test/gitpracticewithbranching.java
+++ src/test/gitpracticewithbranching.java
@@ -4,7 +4,10 @@ public class gitpracticewithbranching {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("this is the line that was added in eclipse");
+	}
+	public void writingMoreChanges() {
+		
 	}
 
 }
